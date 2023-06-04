fff246fb9fef29f83d08ba3de8bc19f1b1f4baca
Joshua Trees <me@jtrees.io>
2023-06-04 21:40:54 +0200

    feat: add nix flake

---
+++ composer.nix
@@ -0,0 +1,225 @@
+{ fetchzip }:
+{
+  "discord-php/http"."v9.1.8" = fetchzip {
+    name = "discord-php/http";
+    url = "https://api.github.com/repos/discord-php/DiscordPHP-Http/zipball/8dc95a63836f7f0161fc5e52e1d9feeaa36b36ba";
+    sha256 = "0ysvg5y04ma5wimn7vz8a7pifmsp3xplqx4giz2k3c0md3l73lg7";
+    extension = "zip";
+  };
+  "discord/interactions"."2.2.0" = fetchzip {
+    name = "discord/interactions";
+    url = "https://api.github.com/repos/discord/discord-interactions-php/zipball/a6fc0c877b75cf5ff5811f2ea69c5cc4ad6ac457";
+    sha256 = "0dqmdkr31bv3l0mh8nyg6ah0pvghdgsgjc30s1wk1914diw49fr6";
+    extension = "zip";
+  };
+  "evenement/evenement"."v3.0.1" = fetchzip {
+    name = "evenement/evenement";
+    url = "https://api.github.com/repos/igorw/evenement/zipball/531bfb9d15f8aa57454f5f0285b18bec903b8fb7";
+    sha256 = "01n3d3pgbdv38ax2hs6lsj3mrh3scki4wgknh68ncw6a4w3ln46z";
+    extension = "zip";
+  };
+  "fig/http-message-util"."1.1.5" = fetchzip {
+    name = "fig/http-message-util";
+    url = "https://api.github.com/repos/php-fig/http-message-util/zipball/9d94dc0154230ac39e5bf89398b324a86f63f765";
+    sha256 = "0lz24n18c7xzl11yk64fwpl54h7grg1h3vi0m8s0h4qzhsbb6ga7";
+    extension = "zip";
+  };
+  "guzzlehttp/psr7"."2.5.0" = fetchzip {
+    name = "guzzlehttp/psr7";
+    url = "https://api.github.com/repos/guzzle/psr7/zipball/b635f279edd83fc275f822a1188157ffea568ff6";
+    sha256 = "1yx6rjz1krdkiw2wrkvyivri4hnh8z6mzjcvxlw1xkdkmxz8ia7l";
+    extension = "zip";
+  };
+  "mollie/polyfill-libsodium"."v1.1.1" = fetchzip {
+    name = "mollie/polyfill-libsodium";
+    url = "https://api.github.com/repos/mollie/polyfill-libsodium/zipball/60351707e048080a2218a97296f88733f050f183";
+    sha256 = "1mrkkb5prxks58i4yq1424hi2a6haadigbza1lnisl1z5mjgz6m8";
+    extension = "zip";
+  };
+  "monolog/monolog"."3.3.1" = fetchzip {
+    name = "monolog/monolog";
+    url = "https://api.github.com/repos/Seldaek/monolog/zipball/9b5daeaffce5b926cac47923798bba91059e60e2";
+    sha256 = "0nvfjlcxdfijwbppg926lj1ilnkc1hab9cwl9lb8yyy179qrwm3r";
+    extension = "zip";
+  };
+  "nesbot/carbon"."2.66.0" = fetchzip {
+    name = "nesbot/carbon";
+    url = "https://api.github.com/repos/briannesbitt/Carbon/zipball/496712849902241f04902033b0441b269effe001";
+    sha256 = "0gnq4px0igqy7zg7wpmg06a1n91hmsq2g44j2l09xnfw2592nk6h";
+    extension = "zip";
+  };
+  "psr/http-factory"."1.0.2" = fetchzip {
+    name = "psr/http-factory";
+    url = "https://api.github.com/repos/php-fig/http-factory/zipball/e616d01114759c4c489f93b099585439f795fe35";
+    sha256 = "12mmvm6y27zmalq9p2d6543rx8xwqp4znrk9yyyqrkprvd8krhdr";
+    extension = "zip";
+  };
+  "psr/http-message"."1.1" = fetchzip {
+    name = "psr/http-message";
+    url = "https://api.github.com/repos/php-fig/http-message/zipball/cb6ce4845ce34a8ad9e68117c10ee90a29919eba";
+    sha256 = "17yq6k8ah84gbg1ai60270aaqzgiaq6hdjgaqrsjallxdykkbrhc";
+    extension = "zip";
+  };
+  "psr/log"."3.0.0" = fetchzip {
+    name = "psr/log";
+    url = "https://api.github.com/repos/php-fig/log/zipball/fe5ea303b0887d5caefd3d431c3e61ad47037001";
+    sha256 = "0zyhpqy419dx6mx44qpdd57ip05v33gw1hwdf98134pi3z7bf6pq";
+    extension = "zip";
+  };
+  "ralouphie/getallheaders"."3.0.3" = fetchzip {
+    name = "ralouphie/getallheaders";
+    url = "https://api.github.com/repos/ralouphie/getallheaders/zipball/120b605dfeb996808c31b6477290a714d356e822";
+    sha256 = "0ncz24gcx9dj8whicyj9kikq6jmjbp3bzfkcxw24zjwb439c8p4a";
+    extension = "zip";
+  };
+  "ratchet/pawl"."v0.4.1" = fetchzip {
+    name = "ratchet/pawl";
+    url = "https://api.github.com/repos/ratchetphp/Pawl/zipball/af70198bab77a582b31169d3cc3982bed25c161f";
+    sha256 = "0ars5gp5mqzacq9ly07y75shknnami0dins51jkd6vs90wan2qhz";
+    extension = "zip";
+  };
+  "ratchet/rfc6455"."v0.3.1" = fetchzip {
+    name = "ratchet/rfc6455";
+    url = "https://api.github.com/repos/ratchetphp/RFC6455/zipball/7c964514e93456a52a99a20fcfa0de242a43ccdb";
+    sha256 = "1rzrhc5skn3m08pw737zxi6n1qll6bhf3nv5m0iazgv9l3vcb1y8";
+    extension = "zip";
+  };
+  "react/cache"."v1.2.0" = fetchzip {
+    name = "react/cache";
+    url = "https://api.github.com/repos/reactphp/cache/zipball/d47c472b64aa5608225f47965a484b75c7817d5b";
+    sha256 = "0lr3lm24bgm4wdadznji23mb4a0ykq3rg5gn4fafwfs2dmrr7kij";
+    extension = "zip";
+  };
+  "react/child-process"."v0.6.5" = fetchzip {
+    name = "react/child-process";
+    url = "https://api.github.com/repos/reactphp/child-process/zipball/e71eb1aa55f057c7a4a0d08d06b0b0a484bead43";
+    sha256 = "1alb8vik0gmi3zpcsz74z9z49hsjs7rz3dyfgi2gg2bqa0ch61jc";
+    extension = "zip";
+  };
+  "react/datagram"."v1.5.0" = fetchzip {
+    name = "react/datagram";
+    url = "https://api.github.com/repos/reactphp/datagram/zipball/e4c5bf8daf44630f0a938f40fe2104b2b76ad2ff";
+    sha256 = "0ijjm48fz7dakn6ya38ckryhl5hp302jw9vj4qswqmpiap7n4313";
+    extension = "zip";
+  };
+  "react/dns"."v1.10.0" = fetchzip {
+    name = "react/dns";
+    url = "https://api.github.com/repos/reactphp/dns/zipball/a5427e7dfa47713e438016905605819d101f238c";
+    sha256 = "17257fpzf5gnkqskq9y4pv6l4ivb3jg15xja79gxpii0s6snaqq1";
+    extension = "zip";
+  };
+  "react/event-loop"."v1.4.0" = fetchzip {
+    name = "react/event-loop";
+    url = "https://api.github.com/repos/reactphp/event-loop/zipball/6e7e587714fff7a83dcc7025aee42ab3b265ae05";
+    sha256 = "14jkyqlb3p00mbbxxmqj3q4gypyiz4dqm1xgwbx71gzqqglwv6zx";
+    extension = "zip";
+  };
+  "react/http"."v1.9.0" = fetchzip {
+    name = "react/http";
+    url = "https://api.github.com/repos/reactphp/http/zipball/bb3154dbaf2dfe3f0467f956a05f614a69d5f1d0";
+    sha256 = "00ndilxipk3cxrqwkpw6m209iqpzqqgm0n2izc3a4pqvajvv6fd8";
+    extension = "zip";
+  };
+  "react/partial"."v3.0.0" = fetchzip {
+    name = "react/partial";
+    url = "https://api.github.com/repos/friends-of-reactphp/partial/zipball/e06f034747561977670607888e0a99fd282308ab";
+    sha256 = "0hfzz74ixcnnlby4cyf1szg78psr56hj86igyh9zkaivdn0d2bs7";
+    extension = "zip";
+  };
+  "react/promise"."v2.10.0" = fetchzip {
+    name = "react/promise";
+    url = "https://api.github.com/repos/reactphp/promise/zipball/f913fb8cceba1e6644b7b90c4bfb678ed8a3ef38";
+    sha256 = "14ihwnw4qpcilj8yjdri98q0gpnccx935yyfl6qnk4hky4gqywxx";
+    extension = "zip";
+  };
+  "react/promise-timer"."v1.9.0" = fetchzip {
+    name = "react/promise-timer";
+    url = "https://api.github.com/repos/reactphp/promise-timer/zipball/aa7a73c74b8d8c0f622f5982ff7b0351bc29e495";
+    sha256 = "1659ikah1kxmsymaswvxdhl2s54ci2asjv58awv095w2y6pyiric";
+    extension = "zip";
+  };
+  "react/socket"."v1.12.0" = fetchzip {
+    name = "react/socket";
+    url = "https://api.github.com/repos/reactphp/socket/zipball/81e1b4d7f5450ebd8d2e9a95bb008bb15ca95a7b";
+    sha256 = "0cv1153x8wlp4pbiy0z1zgydwc5lphkd4l1iq8v1pjflaf2fm0qv";
+    extension = "zip";
+  };
+  "react/stream"."v1.2.0" = fetchzip {
+    name = "react/stream";
+    url = "https://api.github.com/repos/reactphp/stream/zipball/7a423506ee1903e89f1e08ec5f0ed430ff784ae9";
+    sha256 = "03p6wlmf7ybwc0zd3gx907r8qbhjayw4l82pd63r3zh6zsgpvazz";
+    extension = "zip";
+  };
+  "ringcentral/psr7"."1.3.0" = fetchzip {
+    name = "ringcentral/psr7";
+    url = "https://api.github.com/repos/ringcentral/psr7/zipball/360faaec4b563958b673fb52bbe94e37f14bc686";
+    sha256 = "1b25n01ik57gnhm4l0ngpcxpxv4cxzpsddcnjr8bissn5jdy4j6g";
+    extension = "zip";
+  };
+  "simplito/bigint-wrapper-php"."1.0.0" = fetchzip {
+    name = "simplito/bigint-wrapper-php";
+    url = "https://api.github.com/repos/simplito/bigint-wrapper-php/zipball/cf21ec76d33f103add487b3eadbd9f5033a25930";
+    sha256 = "0qwmi88s9010rgw9qhhm58zn50yqb588qagixj1q3kl82vxw1grd";
+    extension = "zip";
+  };
+  "simplito/bn-php"."1.1.3" = fetchzip {
+    name = "simplito/bn-php";
+    url = "https://api.github.com/repos/simplito/bn-php/zipball/189167f940cdb681288a967b0f4d66de81adcd97";
+    sha256 = "1g47137mw4vdzdh3f3p3i5drvl9v7cxgim5fj7rp9jchjgn3xhws";
+    extension = "zip";
+  };
+  "simplito/elliptic-php"."1.0.10" = fetchzip {
+    name = "simplito/elliptic-php";
+    url = "https://api.github.com/repos/simplito/elliptic-php/zipball/a6228f480c729cf8efe2650a617c8500e981716d";
+    sha256 = "1jxn3bs98cwy84lzalka0ssqlrz57f7r4dh5yj1n11bhl0bljnca";
+    extension = "zip";
+  };
+  "symfony/deprecation-contracts"."v3.2.1" = fetchzip {
+    name = "symfony/deprecation-contracts";
+    url = "https://api.github.com/repos/symfony/deprecation-contracts/zipball/e2d1534420bd723d0ef5aec58a22c5fe60ce6f5e";
+    sha256 = "04yakxn0pw4hkf63yy8x016ss6ri52y79v6kwfiv5bcp45jjrjnj";
+    extension = "zip";
+  };
+  "symfony/options-resolver"."v6.2.7" = fetchzip {
+    name = "symfony/options-resolver";
+    url = "https://api.github.com/repos/symfony/options-resolver/zipball/aa0e85b53bbb2b4951960efd61d295907eacd629";
+    sha256 = "0nw9i1l8417c3injk76y9acjgw36mkh23sb5karcarj455489nhs";
+    extension = "zip";
+  };
+  "symfony/polyfill-mbstring"."v1.27.0" = fetchzip {
+    name = "symfony/polyfill-mbstring";
+    url = "https://api.github.com/repos/symfony/polyfill-mbstring/zipball/8ad114f6b39e2c98a8b0e3bd907732c207c2b534";
+    sha256 = "02vgg6saafxp5n1yhkh5pj9jhj45ai8v0pdd2kvima45mbrkfvnc";
+    extension = "zip";
+  };
+  "symfony/polyfill-php80"."v1.27.0" = fetchzip {
+    name = "symfony/polyfill-php80";
+    url = "https://api.github.com/repos/symfony/polyfill-php80/zipball/7a6ff3f1959bb01aefccb463a0f2cd3d3d2fd936";
+    sha256 = "1lla5l1krhc1f5zpgy5ir7g1bhxd5jpgzz3psqxqkm1va1hhhany";
+    extension = "zip";
+  };
+  "symfony/translation"."v6.2.8" = fetchzip {
+    name = "symfony/translation";
+    url = "https://api.github.com/repos/symfony/translation/zipball/817535dbb1721df8b3a8f2489dc7e50bcd6209b5";
+    sha256 = "01dsz0dc999xbah74zp2kynjmxxgvmz8grgmhay71x1p3fkgdhcz";
+    extension = "zip";
+  };
+  "symfony/translation-contracts"."v3.2.1" = fetchzip {
+    name = "symfony/translation-contracts";
+    url = "https://api.github.com/repos/symfony/translation-contracts/zipball/dfec258b9dd17a6b24420d464c43bffe347441c8";
+    sha256 = "0s8vyvz5qgka0w3nx4lzzvlmv7rw4g39mk9ahvvp1bw2dzl0i79r";
+    extension = "zip";
+  };
+  "team-reflex/discord-php"."v7.3.4" = fetchzip {
+    name = "team-reflex/discord-php";
+    url = "https://api.github.com/repos/discord-php/DiscordPHP/zipball/82843da4346878a432171f8d82e345389f7cc49b";
+    sha256 = "0q3c8z5cj671hmzxhq13dvirwd9phw6czi2p7x6j7lx14dsq5ip0";
+    extension = "zip";
+  };
+  "trafficcophp/bytebuffer"."v0.3" = fetchzip {
+    name = "trafficcophp/bytebuffer";
+    url = "https://api.github.com/repos/nesQuick/ByteBuffer/zipball/e94e5c87c41bc79c0f738b0fa89bad11d27ae0b4";
+    sha256 = "194yxdfkmc85zshnk0i6q2lvczp67czzzyrim342cf3bv2malj0d";
+    extension = "zip";
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "Discord bot for DOTA 2 availability developed by Jay";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    composer-nix.url = "github:tristanpemble/composer-nix";
+  };
+
+  outputs = { self, nixpkgs, composer-nix }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      version = "${nixpkgs.lib.substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          composerRepo = composer-nix.lib.mkComposerRepo {
+            inherit system;
+            composerJson = ./composer.json;
+          };
+        in {
+          default = pkgs.stdenvNoCC.mkDerivation {
+            pname = "discord-availability";
+            inherit version;
+
+            src = self;
+
+            COMPOSER = "${composerRepo}/composer.json";
+
+            buildInputs = [ pkgs.php.packages.composer ];
+
+            patchPhase = ''
+              rm -rf ./vendor
+              sed -i '1 i #!${pkgs.php}/bin/php' ./availability.php
+            '';
+
+            buildPhase = ''
+              composer install --no-dev --optimize-autoloader
+            '';
+
+            installPhase = ''
+              mkdir -p "$out"
+              cp -r . "$out/src"
+              chmod +x "$out/src/availability.php"
+
+              mkdir "$out/bin"
+              ln -s "$out/src/availability.php" "$out/bin/discord-availability"
+
+              # Clean up a bit.
+              rm "$out"/src/composer*
+              rm "$out"/src/flake*
+              rm "$out"/src/README.md
+              rm "$out"/src/ruleset.xml
+            '';
+          };
+        }
+      );
+    };
+}
