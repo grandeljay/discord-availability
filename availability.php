edf54782a99f3268d4641d5db8e210a441b2f938
grandeljay <github.jay@grandel.anonaddy.me>
2023-06-05 15:56:06 +0200

    docs: add doc block

--- availability.php
+++ availability.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Discord availability
+ *
+ * Lets you know about players availability for Dota.
+ *
+ * @link OAuth (live) https://discord.com/api/oauth2/authorize?client_id=1100405938801872956&permissions=0&scope=bot%20applications.commands
+ * @link OAuth (test) https://discord.com/api/oauth2/authorize?client_id=1115198941659664397&permissions=0&scope=bot%20applications.commands
+ *
+ * @author Jay Trees <github.jay@grandel.anonaddy.me>
+ */
+
 namespace Grandeljay\Availability;
 
 require __DIR__ . '/vendor/autoload.php';
