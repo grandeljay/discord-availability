915ae2a4454886bca9a3f8bd3d23a0e29fc5398b
Joshua Trees <me@jtrees.io>
2023-06-12 23:40:52 +0200

    feat: validate config file

--- src/Grandeljay/Availability/Config.php
+++ src/Grandeljay/Availability/Config.php
@@ -23,13 +23,41 @@ class Config
             $path = str_replace('$HOME', getenv('HOME'), $path);
 
             if (file_exists($path)) {
-                $contents     = file_get_contents($path);
-                $this->config = json_decode($contents, true);
+                $raw_data    = file_get_contents($path);
+                $parsed_data = json_decode($raw_data, true, 2);
+
+                if ($parsed_data == null) {
+                    die(sprintf("Bad config.json at `%s`: Invalid JSON.\n", $path));
+                }
+
+                $error = $this->validateConfig($parsed_data);
+
+                if ($error) {
+                    die(sprintf("Bad config.json at `%s`: %s\n", $path, $error));
+                }
+
+                $this->config = $parsed_data;
                 return;
             }
         }
 
         die("Missing config.json. Please refer to README.md.\n");
+    }
+
+    /**
+     * Validates the passed config and returns an error if it is invalid.
+     *
+     * @param array $config The config to validate.
+     *
+     * @return string|null A potential error that occurred.
+     */
+    private function validateConfig(array $config): ?string
+    {
+        if (!isset($config['token'])) {
+            return 'Required key "token" is not set.';
+        }
+
+        return null;
     }
 
     /**
