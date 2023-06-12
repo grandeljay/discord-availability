97f9c3926f48c1ae5c76a566a768836ce1d0e596
Joshua Trees <me@jtrees.io>
2023-06-13 00:43:08 +0200

    feat: make 'directory_availabilities' config optional

--- src/Grandeljay/Availability/Config.php
+++ src/Grandeljay/Availability/Config.php
@@ -12,6 +12,8 @@ class Config
         '/etc/discord-availability/config.json',
     );
 
+    private $defaultAvailabilitiesDir = '$HOME/.local/share/discord-availability/availabilities';
+
     public function __construct()
     {
         $this->loadConfig();
@@ -20,7 +22,7 @@ class Config
     private function loadConfig(): void
     {
         foreach ($this->filepaths as $path) {
-            $path = str_replace('$HOME', getenv('HOME'), $path);
+            $path = $this->expandHome($path);
 
             if (file_exists($path)) {
                 $raw_data    = file_get_contents($path);
@@ -57,6 +59,8 @@ class Config
             return 'Required key "token" is not set.';
         }
 
+        // TODO: Validate that configured availabilities directory exists.
+
         return null;
     }
 
@@ -67,7 +71,7 @@ class Config
      *
      * @return mixed
      */
-    public function get(string $key): mixed
+    private function get(string $key): mixed
     {
         if (isset($this->config[$key])) {
             return $this->config[$key];
@@ -84,5 +88,39 @@ class Config
     public function getAPIToken(): string
     {
         return $this->get('token');
+    }
+
+    /**
+     * Returns the path of the availabilities directory.
+     *
+     * @return string
+     */
+    public function getAvailabilitiesDir(): string
+    {
+        if ($this->get('directory_availabilities')) {
+            return $this->normalizePath($this->get('directory_availabilities'));
+        }
+
+        return $this->expandHome($this->defaultAvailabilitiesDir);
+    }
+
+    private function normalizePath(string $path): string
+    {
+        if (str_starts_with($path, '/')) {
+            return $path;
+        }
+
+        $cwd = getcwd();
+
+        if (!$cwd) {
+            die('Could not determine current working directory.');
+        }
+
+        return $cwd . '/' . $path;
+    }
+
+    private function expandHome(string $path): string
+    {
+        return str_replace('$HOME', getenv('HOME'), $path);
     }
 }
